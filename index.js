70905264d728ccc62b115a38743afd43d23247d7
Ubaidullah Omer <121765405+UbaidullahMOmer@users.noreply.github.com>
2024-06-16 22:28:30 +0500

    update

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/product.model");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from node api nodemon");
 })
 
+app.post("/api/products", async (req, res) => {
+  try{
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch(err) {
+    res.status(500).json({message: err.message})
+  }
+})
 
-mongoose.connect("mongodb+srv://researchfigma:Pu6WiSr49RMR0PIx@bookeshdb.y5osrdm.mongodb.net/?retryWrites=true&w=majority&appName=BookeshDB")
+mongoose.connect("mongodb+srv://researchfigma:qdTYAHzs5CRo65f8@backenddb.afhvqcp.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
 .then(() => {
-  console.log("MongoDB connected");
+  console.log("Connect to Database");
+  app.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
 })
 .catch((err) => {
-  console.log(err);
+  console.log("Connection failed!", err);
 })
